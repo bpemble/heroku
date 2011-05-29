6477d22a3d1d7fcb82dde9a6cb6c404bda569b04
Brian Pemble <brian.pemble@gmail.com>
2011-05-29 14:37:04 -0500

    Added Contact page.

--- config/routes.rb
+++ config/routes.rb
@@ -50,8 +50,9 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "home#index"
+  root :to => "index#index"
   match "/about" => "about#about"
+  match "/contact" => "contact#contact"
 
   # See how all your routes lay out with "rake routes"
 
